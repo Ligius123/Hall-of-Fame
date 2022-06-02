4d4c3f59debea72f618424119716e2c7bc96be08
Ligius123 <ligius.matesoni@student.upt.ro>
2022-06-02 12:00:40 +0300

    some touches

--- Frontend/src/Components/Auth/Auth.js
+++ Frontend/src/Components/Auth/Auth.js
@@ -10,12 +10,14 @@ import {
 } from "@material-ui/core";
 import { useHistory } from "react-router-dom";
 // import { GoogleLogin } from "react-google-login";
-// import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+
 
 import { signin, signup } from "../../actions/auth";
 // import { AUTH } from "../../Constants/actionTypes";
 import useStyles from "./styles";
 import Input from "./Input";
+import styles from "./AuthForm.module.css";
 
 const initialState = {
   firstName: "",
@@ -73,65 +75,68 @@ const SignUp = () => {
     setForm({ ...form, [e.target.name]: e.target.value });
 
   return (
-    <Container component="main" maxWidth="xs">
-      <Paper className={classes.paper} elevation={3}>
-        {/* <Avatar className={classes.avatar}>
+    <div
+      className={styles.wrapper}
+    >
+      <Container component="main" maxWidth="xs">
+        <Paper className={classes.paper} elevation={3}>
+          {/* <Avatar className={classes.avatar}> */}
           <LockOutlinedIcon />
-        </Avatar> */}
-        <Typography component="h1" variant="h5">
-          {isSignup ? "Sign up" : "Sign in"}
-        </Typography>
-        <form className={classes.form} onSubmit={handleSubmit}>
-          <Grid container spacing={2}>
-            {isSignup && (
-              <>
-                <Input
-                  name="firstName"
-                  label="First Name"
-                  handleChange={handleChange}
-                  autoFocus
-                  half
-                />
-                <Input
-                  name="lastName"
-                  label="Last Name"
-                  handleChange={handleChange}
-                  half
-                />
-              </>
-            )}
-            <Input
-              name="email"
-              label="Email Address"
-              handleChange={handleChange}
-              type="email"
-            />
-            <Input
-              name="password"
-              label="Password"
-              handleChange={handleChange}
-              type={showPassword ? "text" : "password"}
-              handleShowPassword={handleShowPassword}
-            />
-            {isSignup && (
+        {/* </Avatar> */}
+          <Typography component="h1" variant="h5" className={styles.text}>
+            {isSignup ? "Sign up" : "Sign in"}
+          </Typography>
+          <form className={classes.form} onSubmit={handleSubmit}>
+            <Grid container spacing={2}>
+              {isSignup && (
+                <>
+                  <Input
+                    name="firstName"
+                    label="First Name"
+                    handleChange={handleChange}
+                    autoFocus
+                    half
+                  />
+                  <Input
+                    name="lastName"
+                    label="Last Name"
+                    handleChange={handleChange}
+                    half
+                  />
+                </>
+              )}
               <Input
-                name="confirmPassword"
-                label="Repeat Password"
+                name="email"
+                label="Email Address"
                 handleChange={handleChange}
-                type="password"
+                type="email"
               />
-            )}
-          </Grid>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            {isSignup ? "Sign Up" : "Sign In"}
-          </Button>
-          {/* <GoogleLogin
+              <Input
+                name="password"
+                label="Password"
+                handleChange={handleChange}
+                type={showPassword ? "text" : "password"}
+                handleShowPassword={handleShowPassword}
+              />
+              {isSignup && (
+                <Input
+                  name="confirmPassword"
+                  label="Repeat Password"
+                  handleChange={handleChange}
+                  type="password"
+                />
+              )}
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              {isSignup ? "Sign Up" : "Sign In"}
+            </Button>
+            {/* <GoogleLogin
             clientId="564033717568-e5p23rhvcs4i6kffgsbci1d64r8hp6fn.apps.googleusercontent.com"
             render={(renderProps) => (
               <Button
@@ -150,18 +155,19 @@ const SignUp = () => {
             onFailure={googleError}
             cookiePolicy="single_host_origin"
           /> */}
-          <Grid container justify="flex-end">
-            <Grid item>
-              <Button onClick={switchMode}>
-                {isSignup
-                  ? "Already have an account? Sign in"
-                  : "Don't have an account? Sign Up"}
-              </Button>
+            <Grid container justify="flex-end">
+              <Grid item>
+                <Button onClick={switchMode} className={styles.text}>
+                  {isSignup
+                    ? "Already have an account? Sign in"
+                    : "Don't have an account? Sign Up"}
+                </Button>
+              </Grid>
             </Grid>
-          </Grid>
-        </form>
-      </Paper>
-    </Container>
+          </form>
+        </Paper>
+      </Container>
+    </div>
   );
 };
 
--- Frontend/src/Components/Auth/styles.js
+++ Frontend/src/Components/Auth/styles.js
@@ -1,12 +1,44 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles((theme) => ({
+  // wrapper: {
+  //   margin: "8rem auto",
+  //   backgroundImage: url("LockerRoom.jpg"),
+  //   backgroundRepeat: "no-repeat",
+  //   backgroundSize: cover,
+  //   backgroundPosition: center,
+  //   width: "60%",
+  //   textAlign: center,
+  //   display: flex,
+  //   justifyContent: center,
+  //   borderRadius: "16px",
+  //   boxShadow: "0 0 15px 5px rgba(0, 0, 0, 0.75)",
+  //   maxWidth: "60rem",
+  //   /* width: 90%; */
+  //   margin: "2rem auto",
+  //   animation: "wrapper-appear 1s ease-out forwards",
+  // },
   paper: {
     marginTop: theme.spacing(8),
     display: "flex",
     flexDirection: "column",
-    alignItems: "center",
-    padding: theme.spacing(2),
+    justifyContent: "center",
+    backgroundColor: "#38015c",
+    boxShadow: "0 1px 4px rgba(0, 0, 0, 0.2)",
+    opacity: "0.9",
+    // position: "relative",
+    // padding: theme.spacing(2),
+    padding: "1rem",
+    borderRadius: "6px",
+    margin: "7rem auto",
+    width: "110%",
+    maxWidth: "25rem",
+    // display: "inline-block",
+    // marginLeft: "auto",
+    // marginRight: "auto",
+    // textAlign: "left",
+    // textAlign:"center",
+    // align:"center",
   },
   root: {
     "& .MuiTextField-root": {
